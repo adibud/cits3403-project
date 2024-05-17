0882500a8b3a50b12ed94aa44c62a756fd309392
pohhui247 <168277988+pohhui247@users.noreply.github.com>
2024-05-18 03:06:09 +0800

    Add files via upload
    Add image upload capabilities, still integrating tanmays frontend.

---
+++ Website3.4/webpage/models.py
@@ -0,0 +1,29 @@
+from . import db 
+from flask_login import UserMixin 
+from sqlalchemy import func, TIMESTAMP, text, ForeignKeyConstraint
+
+class requests(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    descr = db.Column(db.String)
+    quantity = db.Column(db.Integer)
+    image = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    com = db.relationship('comments', backref='requests', lazy=True)
+
+
+class users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), unique=True)
+    password = db.Column(db.String(150))
+    username = db.Column(db.String(200), unique=True)
+    req = db.relationship('requests', backref='users', lazy=True)
+    
+   
+class comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), unique=True)
+    time_created = db.Column(db.DateTime, default=func.now(), nullable=False)
+    request_id = db.Column(db.Integer, db.ForeignKey('requests.id'), nullable=False)  # Foreign key to Request
+    commenter_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)  # Foreign key to User
+    commenter = db.relationship('users', backref='comments')
