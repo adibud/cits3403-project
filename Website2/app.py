9d009af9730fc2a6972470fa1527198450a278ad
pohhui247 <168277988+pohhui247@users.noreply.github.com>
2024-05-18 20:43:41 +0800

    Delete Website2 directory
    Old iteration of project. Not Needed

--- Website2/app.py
+++
@@ -1,7 +0,0 @@
-from webpage import create_app
-
-# Create a Flask app
-app = create_app()
-
-if __name__=="__main__":
-    app.run(debug=True)
--- Website2/webpage/main.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, request, redirect, url_for, request, flash
-
-main = Blueprint('main', __name__)
-
-@main.route("/home")
-def homepage():
-    return render_template("base.html")
